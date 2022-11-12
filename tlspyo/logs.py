c4653d2e64cbc593cc3dc51274f8aea26df4f367
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2022-11-12 18:23:19 -0500

    logging

--- tlspyo/logs.py
+++ tlspyo/logs.py
@@ -4,8 +4,11 @@ DEFAULT_FORMAT_STR = "%(levelname)s:%(asctime)s:%(message)s"
 DEFAULT_LEVEL = logging.getLogger().getEffectiveLevel()
 
 logger = logging.getLogger('tlspyo')
-console_handler = logging.StreamHandler()
-default_formatter = logging.Formatter(DEFAULT_FORMAT_STR)
-console_handler.setFormatter(default_formatter)
-logger.addHandler(console_handler)
-logger.setLevel(DEFAULT_LEVEL)
+if not logger.handlers:
+    console_handler = logging.StreamHandler()
+    default_formatter = logging.Formatter(DEFAULT_FORMAT_STR)
+    console_handler.setFormatter(default_formatter)
+    logger.addHandler(console_handler)
+if logger.level == logging.NOTSET:
+    logger.setLevel(DEFAULT_LEVEL)
+logger.propagate = False
