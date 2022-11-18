9efb5e1349c3c62f016a252e03554b4ba212a208
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2022-11-17 22:40:20 -0500

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,9 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../../tlspyo/'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../tlspyo/'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
