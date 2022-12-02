71f78976b658679794ce622db78cdd9d38a5fb77
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2022-12-01 23:04:38 -0500

    Release 0.2.3

--- tlspyo/utils.py
+++ tlspyo/utils.py
@@ -2,19 +2,23 @@ import signal
 import queue
 
 
+try:
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+except Exception as e:
+    pass
+
+
 def wait_event(event):
     """
     Workaround for an Event bug on Windows.
 
     See: https://bugs.python.org/issue35935
     """
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
     event.wait()
 
 
 def get_from_queue(q, blocking=False):
     try:
-        signal.signal(signal.SIGINT, signal.SIG_DFL)
         return [q.get(block=blocking)]
     except queue.Empty:
         return []
